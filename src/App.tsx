b6d67ef094c58de45171b5bf9b7855cc47fc29ad
Havrylenko Olexii <hristinson@gmail.com>
2025-09-05 15:56:25 +0300

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Do Not Edit <code>src/App.tsx</code> and save to reload.
+          ЮЛЯ!.....ЮЛЯ!........ ;-0
         </p>
         <a
           className="App-link"
