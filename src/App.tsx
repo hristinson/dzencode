4c93c4f12a3b9d187237189c50e8a7898a8db469
Havrylenko Olexii <hristinson@gmail.com>
2025-09-07 14:44:51 +0300

    Add version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { addProduct } from "./app";
 import ProductList from "./components/products_list";
+import packageInfo from "../package.json";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Frontend </p>
+        <p>Frontend {packageInfo.version}</p>
         <ProductList />
         <button onClick={() => addProduct()}>Add</button>
       </header>
