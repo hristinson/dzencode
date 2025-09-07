a914377abb3a801190453c78e2dfb7c3b5432d5c
Havrylenko Olexii <hristinson@gmail.com>
2025-09-07 15:13:11 +0300

    process.env.MONGODB_URI

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,8 +1,10 @@
 // returnTrue.js
+import dotenv from "dotenv";
+dotenv.config();
 
 export const handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ result: true }),
+    body: JSON.stringify({ result: process.env.MONGODB_URI }),
   };
 };
