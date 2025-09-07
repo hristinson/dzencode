9c772a705b76f335c6c8bed20c6027ab7ccc709c
Havrylenko Olexii <hristinson@gmail.com>
2025-09-07 20:10:12 +0300

    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./i18n";
 import ProductList from "./components/products_list";
-import { useCallback, useState } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import "./index.scss";
